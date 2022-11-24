654e2904a41dce34401926c0d8194a9e618c52e9
gyojincompany <luckyred@gmail.com>
2022-11-24 10:28:12 +0900

    index 페이지 레이아웃 완료

---
+++ src/main/java/com/gyojincompany/home/Myprofilehome20221124Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myprofilehome20221124Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myprofilehome20221124Application.class, args);
+	}
+
+}
